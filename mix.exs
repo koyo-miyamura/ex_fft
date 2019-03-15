0bd46e231f461c005752e18487deb56b8d846a0c
koyo <nexus9071@gmail.com>
2019-03-15 20:48:09 +0900

    hex用に調整と適当にライブラリの導入

--- mix.exs
+++ mix.exs
@@ -6,6 +6,12 @@ defmodule ExFft.MixProject do
       app: :ex_fft,
       version: "0.1.0",
       elixir: "~> 1.7",
+      description: "Elixir's FFT (Fast Fourier Transform) library (not FINAL FANTASY TACTICS)",
+      package: [
+        maintainers: ["koyo"],
+        licenses: ["Apache 2.0"],
+        links: %{"GitHub" => "https://github.com/koyo-miyamura/ex_fft"}
+      ],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,6 +29,10 @@ defmodule ExFft.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:complex_num, "~> 1.1"},
+      {:math, "~> 0.3.0"},
+      {:ex_doc, "~> 0.19.3", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 0.9", only: :dev, runtime: false}
     ]
   end
 end
